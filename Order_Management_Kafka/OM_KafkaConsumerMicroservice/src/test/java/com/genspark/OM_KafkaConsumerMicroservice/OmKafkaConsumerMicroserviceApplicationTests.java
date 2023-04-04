664fc2d7f3120bdc68e141b84c93ab12b911b709
hana <hanane7885@gmail.com>
2023-04-04 18:38:50 -0500

    project structure

---
+++ Order_Management_Kafka/OM_KafkaConsumerMicroservice/src/test/java/com/genspark/OM_KafkaConsumerMicroservice/OmKafkaConsumerMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.genspark.OM_KafkaConsumerMicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OmKafkaConsumerMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ Order_Management_Kafka/OM_KafkaProducerMicroservice/src/test/java/com/genspark/OM_KafkaProducerMicroservice/OmKafkaProducerMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.genspark.OM_KafkaProducerMicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OmKafkaProducerMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
